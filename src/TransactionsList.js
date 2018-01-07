3eefaad6697c799bd19f7364d5d57adf56efb58c
github username <github email address>
2018-01-07 17:56:42 -0500

    Done.

---
+++ src/TransactionsList.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import Transaction from './Transaction'
+
+const TransactionsList = (props) => {
+
+ let transactionComponents = props.transactions.map(
+   transaction => <Transaction transaction={transaction}/>)
+
+  return (
+    <table className="ui celled striped padded table">
+      <tbody>
+        <tr>
+          <th>
+            <h3 className="ui center aligned header">
+              Posted At
+            </h3>
+          </th>
+          <th>
+            <h3 className="ui center aligned header">
+              Description
+            </h3>
+          </th>
+          <th>
+            <h3 className="ui center aligned header">
+              Category
+            </h3>
+          </th>
+          <th>
+            <h3 className="ui center aligned header">
+              Amount
+            </h3>
+          </th>
+        </tr>
+              {transactionComponents}
+      </tbody>
+    </table>
+  )
+}
+
+export default TransactionsList
